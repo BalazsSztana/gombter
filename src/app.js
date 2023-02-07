8375d9014e460640dea229da0d3d420024019cd5
Sztana Balázs <balazssztana@outlook.com>
2023-02-07 09:53:03 +0100

    Végső javítások elvégezve

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 * Author: Sztana Balázs
 * Copyright: 2023, Sztana Balázs
 * Group: Szoft I-2-E
-* Date: 2023-01-26
+* Date: 2023-02-07
 * Github: https://github.com/BalazsSztana/
 * Licenc: GNU GPL
 */
